1005fda53981510ad35c277af39fa6336d683604
Harmish77 <107766922+Harmish77@users.noreply.github.com>
2023-08-19 16:09:40 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -66,7 +66,7 @@ async def start_services():
     print('\n')
     print('---------------------------------------------------------------------------------------------------------')
     print('---------------------------------------------------------------------------------------------------------')
-    print('Join https://t.me/HappyKidBGMZ  to follow me for new bots')
+    print('Join https://t.me/main_movie_hub  to follow me for new bots')
     print('---------------------------------------------------------------------------------------------------------')
     print('\n')
     print('----------------------- Service Started -----------------------------------------------------------------')
@@ -76,7 +76,7 @@ async def start_services():
     if Var.ON_HEROKU:
         print('                        app runnng on =>> {}'.format(Var.FQDN))
     print('---------------------------------------------------------------------------------------------------------')
-    print('Give a star to my repo https://github.com/HappyKidBGMZ/BGM-File-To-Link  also follow me for new bots')
+    print('Give a star to my repo https://github.com/Harmish77  also follow me for new bots')
     print('---------------------------------------------------------------------------------------------------------')
     await idle()
 
